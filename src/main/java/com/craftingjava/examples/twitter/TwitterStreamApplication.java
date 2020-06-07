79fb66ad291f2127e202b048a5838b814abb1404
root <arellano.gustavo@gmail.com>
2020-06-07 14:06:28 -0500

    Remove "Exception" from main

--- src/main/java/com/craftingjava/examples/twitter/TwitterStreamApplication.java
+++ src/main/java/com/craftingjava/examples/twitter/TwitterStreamApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TwitterStreamApplication {
 
-  public static void main(String[] args) throws Exception {
+  public static void main(String[] args) {
     SpringApplication.run(TwitterStreamApplication.class, args);
   }
 
